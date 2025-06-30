a8dbd36a544ce9687ab160b9f6d8d8c17a1ed294
KAFKHA YASIN A <kafkhayasin@gmail.com>
2025-06-30 22:32:51 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import autoprefixer from "autoprefixer";
 
 export default defineConfig({
     build: {
-        outDir: "public/build",
+        outDir: "dist",
         manifest: true,
     },
     plugins: [
